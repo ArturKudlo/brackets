1e0032b65e699a0aa83e630aada5c50fb20132e4
Artur Kudla <arturkapes@gmail.com>
2019-03-12 14:13:08 +0300

    fix: change codestyle

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
+  if(str.length%2 != 0)return false;
+  if(str.length == 0)return false;      
   var all = [];
-  str = str.split('');
-  if (str.length % 2 !== 0) return false;
-  str.forEach(function(char) {
-  if (char == '(' ) all.push(char);
-  if (char == ')' && all[all.length-1] == '(') all.pop();
-  if (char == '[' ) all.push(char);
-  if (char == ']' && all[all.length-1] == '[') all.pop();
-  if (char == '{' ) all.push(char);
-  if (char == '}' && all[all.length-1] == '{') all.pop();
-  });
+  for(var i = 0; i < str.length; i++){
+    for(var k = 0; k < bracketsConfig.length; k++){
+      var extreme = all[all.length-1];   
+      if(str[i]==bracketsConfig[k][1] && all.length != 0 && extreme == bracketsConfig[k][0]){
+        all.pop(); 
+      }
+      else if (str[i] == bracketsConfig[k][0]){
+        all.push(str[i]);
+      }
+    }
+  } 
   return all.length === 0;
-  };
+};
