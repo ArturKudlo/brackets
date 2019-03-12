7863dd84c801f3e4093759690b457191a890793a
Artur Kudla <arturkapes@gmail.com>
2019-03-12 13:31:49 +0300

    feature: add brackets logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var all = [];
+  str = str.split('');
+  if (str.length % 2 !== 0) return false;
+  str.forEach(function(char) {
+  if (char == '(' ) all.push(char);
+  if (char == ')' && all[all.length-1] == '(') all.pop();
+  if (char == '[' ) all.push(char);
+  if (char == ']' && all[all.length-1] == '[') all.pop();
+  if (char == '{' ) all.push(char);
+  if (char == '}' && all[all.length-1] == '{') all.pop();
+  });
+  return all.length === 0;
+  };
